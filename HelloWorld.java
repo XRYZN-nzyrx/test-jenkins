84eadf50107588974b4d8ef4abb982f0b18f2302
Aryan Sahani <spookywheels2020@gmail.com>
2026-02-16 10:46:42 +0530

    Changes Made (2)

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,8 +5,11 @@ class HelloWorld
 	{
 		System.out.println("Hello World!");
 		int a=10;
-		int b=20;
+		int b=60;
 		int c=a+b;
 		System.out.println("The sum of a and b is: "+c);
+		System.out.println("The product of a and b is: "+(a*b));
+		System.out.println("The difference of a and b is: "+(a-b));		
+		
 	}
 }
