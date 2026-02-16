4757ce61a01cca5bc9a31dd52526669b0c357bd9
Aryan Sahani <spookywheels2020@gmail.com>
2026-02-16 10:44:07 +0530

    Changes made

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,12 @@
-import java.util.*;
+//import java.util.*;
 class HelloWorld
 {
 	public static void main(String args[])
 	{
 		System.out.println("Hello World!");
+		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println("The sum of a and b is: "+c);
 	}
 }
